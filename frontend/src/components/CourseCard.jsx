316c171ed740980a62779da028040f157fad4c38
= <=>
2026-01-09 11:23:37 +0100

    nie dziala czcionka tytulu

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -7,7 +7,7 @@ function CourseCard({ course }) {
   return (
     <article className={styles['courseCard']}>
       <figure className={styles['courseCardFigure']}>
-        <img className={styles['courseImage']} alt="img" loading="lazy" width="1280" height="800" decoding="async" src="https://sklep.szurek.tv/img/bezpieczny_programista.png"></img>
+        <img className={styles['courseImage']} alt="img" loading="lazy" width="1280" height="800" decoding="async" src="phttps://sklep.szurek.tv/img/bezpieczny_programista.png"></img>
         {/* <img src={course.img} alt={course.name} className="courseCardImg"/> */}
       </figure>
       <div>
