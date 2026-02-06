53068e720fc2d9eca27f203660533b83adca3439
Bartlomiej-szkola <Bartlomiej.Tobiasz@tm1.edu.pl>
2026-02-06 10:49:48 +0100

    zmiana bazy na xampp

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2/promise';
 
 export const db_conn = await mysql.createConnection({
-    host: "sql7.freesqldatabase.com",
-    user: "sql7814456",
-    password: "t14dZnNLyt",
-    database: "sql7814456" 
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "baza" 
 })
