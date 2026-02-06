e37a32d06f4867e884296a1c43b5de7ec3e0790d
Bartlomiej-szkola <Bartlomiej.Tobiasz@tm1.edu.pl>
2026-02-06 12:58:07 +0100

    Rejestracja

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import ProtectedPage from './components/ProtectedPage';
 import MainPage from "./components/MainPage";
+import Registration from './components/Registration';
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Registration />} />
                 <Route path="/protected" element={<ProtectedPage />} />
             </Routes>
         </Router>
--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
-const Login = () => {
+const Registration = () => {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
-        const token = Cookies.get('token');
-        if (token) {
-            navigate('/protected');
-        }
-    }, [navigate]);
+            const token = Cookies.get('token');
+            if (token) {
+                navigate('/protected');
+            }
+        }, [navigate]);
 
-    const handleLogin = async (e) => {
+    const handleRegistration = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:3000/login', { name, password });
-            Cookies.set('token', response.data.token, { expires: 1 }); // 1 dzień
+            const response = await axios.post('http://localhost:3000/add-user', { name, password });
+            Cookies.set('token', response.data.token, { expires: 1 });
             navigate('/protected');
         } catch (error) {
-            setErrorMessage('Niepoprawne dane logowania');
+            setErrorMessage('Wystąpił błąd podczas rejestracji');
         }
     };
 
     return (
         <div>
-            <h2>Logowanie</h2>
-            <form onSubmit={handleLogin}>
+            <h2>Rejestracja</h2>
+            <form onSubmit={handleRegistration}>
                 <div>
                     <label>Nazwa użytkownika:</label>
                     <input 
@@ -47,12 +47,12 @@ const Login = () => {
                         onChange={(e) => setPassword(e.target.value)} 
                     />
                 </div>
-                <button type="submit">Zaloguj się</button>
+                <button type="submit">Zarejestruj się</button>
                 {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
             </form>
-            <p>Nie masz konta? <button onClick={() => navigate('/register')}>Zarejestruj się</button></p>
+            <p>Masz już konto? <button onClick={() => navigate('/login')} >Zaloguj się</button></p>
         </div>
     );
 };
 
-export default Login;
+export default Registration;
