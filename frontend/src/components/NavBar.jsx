3662cafd965d38c631659205ab0031cd8eee0cf4
ASalek-Szkola <antoni.salek@tm1.edu.pl>
2026-02-09 12:34:11 +0100

    Nowa ikonka koszyka

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,10 +2,13 @@ import Cookies from 'js-cookie';
 
 import styles from './NavBar.module.css';
 import DarkModeGifSwitch from "./DarkModeGifSwitch";
-import ShoppingCartIcon from './ShoppingCartIcon';
+import ShoppingCartSVG from '../assets/shopping-cart.svg?react';
 import UserIconSVG from '../assets/user-icon.svg?react';
 
 export default function NavBar() {
+    const iconWidth = parseFloat(window.getComputedStyle(document.body).getPropertyValue('--icon-width'));
+    const iconHeight = parseFloat(window.getComputedStyle(document.body).getPropertyValue('--icon-height'));
+
     return (<>
         <div className={styles['wrapper']}>
             <nav className={styles["navbar"]}>
@@ -16,21 +19,23 @@ export default function NavBar() {
                 </div>
 
                 <div className={styles['nav-links']}>
-                    <ShoppingCartIcon />
+                    <div className={styles['shopping-cart-icon']}>
+                        <ShoppingCartSVG width={iconWidth} height={iconHeight} />
+                    </div>
 
                     <DarkModeGifSwitch />
-                        
-                    {!Cookies.get("token") && 
+
+                    {!Cookies.get("token") &&
                         <div className={styles['login-btn']}>
                             <a href='/login'>
                                 Zaloguj się
                             </a>
                         </div>
                     }
-                    {Cookies.get("token") && 
-                    <div className={styles['user-icon']} >
-                        <UserIconSVG width={parseFloat(window.getComputedStyle(document.body).getPropertyValue('--icon-width'))} height={parseFloat(window.getComputedStyle(document.body).getPropertyValue('--icon-height'))} />
-                    </div>
+                    {Cookies.get("token") &&
+                        <div className={styles['user-icon']} >
+                            <UserIconSVG width={iconWidth} height={iconHeight} />
+                        </div>
                     }
                 </div>
             </nav>
