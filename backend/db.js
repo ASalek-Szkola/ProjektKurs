9348b60fdf639e077bbc191e0d30d22db48a0af8
Bartlomiej-szkola <Bartlomiej.Tobiasz@tm1.edu.pl>
2026-02-11 11:51:53 +0100

    update login

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const db_conn = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "baza" 
+    host: "projektkurs.mysql.database.azure.com",
+    user: "projekt",
+    password: "Warszawa2026!",
+    database: "baza",
+    port: "3306"
 })
